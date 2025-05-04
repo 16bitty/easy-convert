7d53d8b8df521872fb179bccff6905cc1f0f1f37
16bitty <max@koeninger.org>
2025-05-04 01:10:53 -0500

    Create converter setup skeleton

--- audio_converter.py
+++ audio_converter.py
@@ -1,4 +1,27 @@
+import ffmpeg
+
+from config import ConfigHandler
+
+
 class AudioConverter:
 
-    def __init(self):
+    def __init__(self):
         self.config = None
+        self.input_dir = ""
+        self.output_dir = ""
+
+    def setup(self, input_dir="", output_dir="", from_codec="", to_codec=""):
+        if self.config is None:
+            print("Loading config...")
+            conf_handler = ConfigHandler()
+            try:
+                self.config = conf_handler.load_config()
+                print("Config loaded.")
+            except:
+                print("Error loading config. Please check config.toml.")
+                return
+        elif (input_dir, output_dir, from_codec, to_codec != ""):
+            print("Converter initialized..")
+        else:
+            print("Could not find necessary inputs for conversion. This should never happen.")
+
